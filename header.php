d45f1d514e53f37882a7d781ab7a2b0312e80dbf
kervisvasquez1993 <kervisvasquez24@gmail.com>
2020-12-25 18:36:30 -0400

    first commit

---
+++ header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+
+<header class="site-header">
+	<div class="container">
+		<div class="barra-navegacion">
+			<div class="logo">
+				<img src="<?php echo get_template_directory_uri();?>/img/logo.svg" alt="logo sitio ">
+			</div>
+			<nav>
+				<?php
+
+					$args = array(
+						'theme_location'  => 'menu-1',
+						'container'       =>  'nav',
+						'container_class' => 'menu_principal_css'
+					);
+					wp_nav_menu($args);
+				?>
+			</nav>
+		</div>
+	</div>
+</header>
+	
+</body>
+</html>
