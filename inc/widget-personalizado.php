631430ea12dc5e96879e6184a3ff67945a8e3772
kervisvasquez1993 <kervisvasquez24@gmail.com>
2021-01-07 14:20:33 +0100

    agregamos widget personalizado pero sin completar la funcionalidad

--- inc/widget-personalizado.php
+++ inc/widget-personalizado.php
@@ -1,12 +1,8 @@
 <?php
-/**
- * Adds Ksk_agency_class widget.
- */
+
 class Ksk_agency_class extends WP_Widget {
 
-	/**
-	 * Register widget with WordPress.
-	 */
+	
 	function __construct() {
 		parent::__construct(
 			'Ksk_agency_class', // Base ID
@@ -14,54 +10,70 @@ class Ksk_agency_class extends WP_Widget {
 			array( 'description' => esc_html__( 'Un widget para ksk_agency', 'ksk_agency' ), ) // Args
 		);
 	}
-
-	/**
-	 * Front-end display of widget.
-	 *
-	 * @see WP_Widget::widget()
-	 *
-	 * @param array $args     Widget arguments.
-	 * @param array $instance Saved values from database.
-	 */
 	public function widget( $args, $instance ) {
 		echo $args['before_widget'];
 		if ( ! empty( $instance['title'] ) ) {
 			echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ) . $args['after_title'];
 		}
-		echo esc_html__( 'Hello, World!', 'ksk_agency' );
+
+		$cantidad = $instance['cantidad'];
+		?>
+		<ul>
+			<?php 
+				$args = array(
+					'post_type' => 'ksk_agency',
+					'posts_per_page' => $cantidad,
+					'orderby' => 'rand'
+				);
+
+				$proyecto = new WP_Query($args);
+
+				while($proyecto->have_posts()):
+					$proyecto->the_post();
+					?>
+						<li class="proyecto-sidebar">
+							<div class="imagen">
+								<?php the_post_thumbnail("thumbnail");?>
+							</div>
+							<div class="contenido-proyecto">
+								<a href="<?php the_permalink();?>">
+									<h3><?php the_title();?></h3>
+								</a>
+							</div>
+						</li>
+					<?php
+					
+				endwhile;	wp_reset_postdata();
+			?>
+		
+		</ul>
+		
+		<?php
 		echo $args['after_widget'];
 	}
-
-	/**
-	 * Back-end widget form.
-	 *
-	 * @see WP_Widget::form()
-	 *
-	 * @param array $instance Previously saved values from database.
-	 */
 	public function form( $instance ) {
-		$title = ! empty( $instance['title'] ) ? $instance['title'] : esc_html__( 'New title', 'ksk_agency' );
-		?>
+		$cantidad = !empty($instance['cantidad']) ? $instancia['cantidad'] : esc_html__( "Cuantos Proyectos deseas Mostrar", "ksks_agency" );
+	?>
+
 		<p>
-		<label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_attr_e( 'Title:', 'ksk_agency' ); ?></label> 
-		<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
+			<label for="<?echo esc_attr($this->get_field_id('cantidad'));?>">
+				<?php esc_attr_e( "Cuantos Proyectos deseas mostrar", "ksk_agency");?>
+			</label>
+			<input 
+			   class="widefat"
+			   id="<?php echo esc_attr($this->get_field_id('cantidad'));?>"
+			   name="<?php echo esc_attr($this->get_field_name('cantidad'));?>"
+			   value = "<?php echo esc_attr( $cantidad );?>"
+			   type="number"
+			   >
 		</p>
 		<?php 
 	}
 
-	/**
-	 * Sanitize widget form values as they are saved.
-	 *
-	 * @see WP_Widget::update()
-	 *
-	 * @param array $new_instance Values just sent to be saved.
-	 * @param array $old_instance Previously saved values from database.
-	 *
-	 * @return array Updated safe values to be saved.
-	 */
+	
 	public function update( $new_instance, $old_instance ) {
 		$instance = array();
-		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? sanitize_text_field( $new_instance['title'] ) : '';
+		$instance['cantidad'] = ( ! empty( $new_instance['cantidad'] ) ) ? sanitize_text_field( $new_instance['cantidad'] ) : '';
 
 		return $instance;
 	}
