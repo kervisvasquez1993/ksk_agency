f97b6c719a200f154186c70e141e37181e39a74d
kervisvasquez1993 <kervisvasquez24@gmail.com>
2021-01-28 21:34:10 -0400

    agregamos page de category para filtrar post

--- home.php
+++ home.php
@@ -18,37 +18,13 @@ get_header();
 
 <main class="pagina seccion no-sidebars cotenedor">
     <ul class="listado-blog">
-            <?php while(have_posts()): the_post();?>
-                <li class="card gradient">
-                    <?php the_post_thumbnail('mediano');?>
-                    <?php the_category();?>
-                    <div class="contenido">
-                        <a href="<?php the_permalink();?>">
-                            <h3><?php the_title();?></h3>
-                        </a>
-                        <p class="meta">
-                            <span>Por:  </span>
-                            <a href="<?php echo get_author_posts_url(get_the_author_meta('ID'));?>">
-                                <?php echo get_the_author_meta("display_name");?>
-                            </a>
-                           
-                        </p>
-                        <p class="meta">
-                            <span>Fecha: </span>
-                            <?php the_time( get_option('data_format') );?> 
-                        </p>
-                    </div>
-                
-                </li>
-
-
-            <?php endwhile;?>
+            <?php get_template_part('template-parts/loop', 'blog');?>
     
     </ul>
 </main>
 
 
-<?
+<?php
 
 get_footer();
     
