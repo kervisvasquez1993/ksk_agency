20477c4519a8278866c2b2872456b5144dd07dd7
kervisvasquez1993 <kervisvasquez24@gmail.com>
2021-03-10 19:33:16 -0400

    style desde from page

--- front-page.php
+++ front-page.php
@@ -1,3 +1,47 @@
 <?php get_header('front');?>
+<section class=" bienvenida text-center section">
+    <h2 class="texto-primario"> <?php the_field('encabezado_bienvenida');?> </h2>
+    <p><?php the_field('texto_bienvenida')?></p>
+</section>
 
+<div class="seccion-areas">
+    <ul class="contenido-areas">
+       <li class="area">
+            <?php 
+                $area1 = get_field('area2');
+                $imagen = wp_get_attachment_image_src($area1['area_imagen'], 'medianos')[0];
+                
+            ?>
+            <img src="<?php echo esc_attr($imagen);?>">
+            <p> <?php echo esc_html( $area1['texto'] )?></p>
+        </li>
+        <li class="area">
+            <?php 
+                $area2 = get_field('area3');
+                $imagen = wp_get_attachment_image_src($area2['area_imagen'], 'medianos')[0];
+                
+            ?>
+            <img src="<?php echo esc_attr($imagen);?>">
+            <p> <?php echo esc_html( $area1['texto'] )?></p>
+        </li>
+        <li class="area">
+            <?php 
+                $area3 = get_field('area4');
+                $imagen = wp_get_attachment_image_src($area3['area_imagen'], 'medianos')[0];
+                
+            ?>
+            <img src="<?php echo esc_attr($imagen);?>">
+            <p> <?php echo esc_html( $area1['texto'] )?></p>
+        </li>
+        <li class="area">
+            <?php 
+                $area4 = get_field('area5');
+                $imagen = wp_get_attachment_image_src($area4['area_imagen'], 'medianos')[0];
+                
+            ?>
+            <img src="<?php echo esc_attr($imagen);?>">
+            <p> <?php echo esc_html( $area1['texto'] )?></p>
+        </li>
+    </ul>
+</div>
 <?php get_footer();?>
